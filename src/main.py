2094a37b2160d5f82d7f287ac445068bc0dac8a1
Santeri Kuusela <santeri02.kuusela@gmail.com>
2023-04-18 16:36:59 +0300

    added more tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 import gui
 
 
-# you only have to run this
-# when running first time you should close the program
-# after closing the program if-statement will run and add testing accounts
-# you should change True value to False not to make duplicates in database
-# to erase the database use function database.delete_database()
+
 
 gui.start()
--- src/tests/functions_test.py
+++ src/tests/functions_test.py
@@ -1,4 +1,3 @@
-import unittest
 import functions
 
 
@@ -6,3 +5,20 @@ def test_new_password():
     assert functions.new_password("moi", "moi") == None
     assert functions.new_password("moimoimoi", "moimoi") == False
     assert functions.new_password("moimoimoi", "moimoimoi") == True
+
+def test_login():
+    assert functions.login("moi", "moi") == None
+    assert functions.login("user1", "moi") == False
+    assert functions.login("user1", "salasana1") == True
+
+def test_new_username():
+    assert functions.login("user1") == False
+    assert functions.login("moi") == True
+
+def test_new_coursetag():
+    assert functions.new_coursetag("ohpe2021") == False
+    assert functions.new_coursetag("ohpe2020") == True
+
+def test_new_studentrole_request():
+    assert functions.new_studentrole_request("moi") == False
+    assert functions.new_studentrole_request("user1") == True
